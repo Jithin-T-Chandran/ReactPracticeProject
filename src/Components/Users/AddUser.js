e4c8c36a62f48a07e63fbeb0e8e7717f86ca408b
Jithin-TChandran <jithin.chandran@auzzio.in>
2022-09-02 22:24:30 +0530

    Updated

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -7,14 +7,15 @@ import classes from "./AddUser.module.css";
 function AddUser(props) {
   const [enteredUserName, setEnteredUserName] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [enteredCollege, setEnteredCollege] = useState("");
   const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if (enteredUserName.trim().length === 0 || enteredAge.trim().length === 0) {
+    if (enteredUserName.trim().length === 0 || enteredAge.trim().length === 0 || enteredCollege.trim().length === 0) {
         setError({
             title:"Invalid input",
-            message:"Please enter a valid name and age (non-empty values)."
+            message:"Please enter a valid name, age (non-empty values) and college name."
         });
         return;
     }
@@ -25,15 +26,19 @@ function AddUser(props) {
         });
       return;
     }
-    props.onAddUser(enteredUserName, enteredAge);
+    props.onAddUser(enteredUserName, enteredAge,enteredCollege);
     setEnteredUserName("");
     setEnteredAge("");
+    setEnteredCollege("");
   };
   const usernameChangeHandler = (event) => {
     setEnteredUserName(event.target.value);
   };
   const ageChangeHandler = (event) => {
     setEnteredAge(event.target.value);
+  };
+  const collegenameChangeHandler = (event) => {
+    setEnteredCollege(event.target.value);
   };
   const errorHandler = () =>{
     setError(null);
@@ -57,6 +62,13 @@ function AddUser(props) {
             type="number"
             value={enteredAge}
             onChange={ageChangeHandler}
+          />
+          <label htmlFor="collegename">College name</label>
+          <input
+            id="collegename"
+            type="text"
+            value={enteredCollege}
+            onChange={collegenameChangeHandler}
           />
           <Button type="submit">Add User</Button>
         </form>
